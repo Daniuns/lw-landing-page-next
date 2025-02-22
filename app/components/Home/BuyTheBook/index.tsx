07294c3cf9f56bf8ae37bbf277debba80f806045
Daniel <daniel.ferreira@octadesk.com>
2025-02-21 22:06:04 -0300

    feat: first commit (but almost done)

---
+++ app/components/Home/BuyTheBook/index.tsx
@@ -0,0 +1,48 @@
+import book from "@/app/assets/images/book.jpg";
+import Image from "next/image";
+import amzSvg from "@/app/assets/images/amazon.svg";
+
+export default function BuyTheBook() {
+  return (
+    <div className="grid grid-cols-1 gap-6 lg:grid-cols-2 lg:gap-32 ">
+      <div className="min-w-40 max-w-sm lg:max-w-md h-auto rounded-xl justify-self-center lg:justify-self-end self-start">
+        <Image className="rounded-xl" src={book} alt="Book" />
+      </div>
+      <div className="self-start w-full xl:w-[80%]">
+        <h2>Sinopse</h2>
+        <h4 className="text-gray-400 text-justify">
+          Quem diria que ano após ano, se tornaria cada vez mais comum encontrar
+          restos de monstros expostos nos mercados da cidade. Seja para o
+          consumo da carne de uma cobra nokai, para utilização da concha de um
+          splavos ou até mesmo para ostentar a cabeça de um pyke na sala de um
+          nobre. Desde que o mundo foi tomado por monstros, não são muitos que
+          se aventuram fora das proteções do reino de Mondy para caçar tais
+          criaturas. Mas Lewis Helderish sente que nasceu para isso. Treinou a
+          vida toda ao lado de Joshua, seu melhor amigo, para abater monstros,
+          conquistar riquezas e desbravar o desconhecido. O que ele não
+          imaginava, é que o desconhecido lhe reservava uma amarga surpresa.
+          Mesmo muito novo, o peso e a responsabilidade de mudar o curso de uma
+          guerra lhe é jogado nas mãos. Correndo contra o tempo, Lewis precisa
+          superar suas ignorâncias quanto à magia e recuperar o pergaminho
+          perdido. Mas o tempo urge, o necromante marcha trazendo a morte
+          consigo para pôr fim às últimas forças da humanidade.
+        </h4>
+        <a
+          href="https://a.co/d/fWogqk1"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="w-full sm:w-auto px-6 flex items-center justify-center gap-2 py-2 mt-4 text-white bg-amber-700 border-2 border-amber-800 rounded-lg transition duration-300 hover:bg-amber-800 hover:border-amber-900 hover:scale-105 focus:outline-none focus:ring-2 focus:ring-amber-600"
+        >
+          <Image
+            width={20}
+            height={20}
+            className="rounded-xl"
+            src={amzSvg}
+            alt="Amazon-logo"
+          />
+          Comprar
+        </a>
+      </div>
+    </div>
+  );
+}
---
+++ app/components/Home/ThemeSection/index.tsx
@@ -0,0 +1,24 @@
+import ThemeSectionItem from "./ThemeSectionItem";
+
+export default function ThemeSection() {
+  return (
+    // center items in grid
+    <div className="grid grid-cols-1 gap-8 md:grid-cols-3 place-items-center">
+      <ThemeSectionItem
+        title="Aventura"
+        subTitle="Uma aventura épica"
+        description="Em uma terra desolada pela destruição, Lewis Helderish busca encontrar uma forma de entender o que aconteceu com seu mundo"
+      />
+      <ThemeSectionItem
+        title="Magias"
+        subTitle="Sistema de magias único inspirado em RPGs"
+        description="Magias elementais e magias sagradas. E talvez... algo a mais (No Spoilers)"
+      />
+      <ThemeSectionItem
+        title="Monstros"
+        subTitle="Criaturas de diversas mitologias"
+        description="Monstros de diversas mitologias, como também criaturas originais"
+      />
+    </div>
+  );
+}
