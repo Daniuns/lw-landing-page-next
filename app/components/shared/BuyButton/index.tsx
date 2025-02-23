c4014e7f9c2de0a6d48ca52b5b8958b10a4539b6
Daniel <daniel.ferreira@octadesk.com>
2025-02-23 20:41:45 -0300

    feat: add both events

--- app/components/shared/BuyButton/index.tsx
+++ app/components/shared/BuyButton/index.tsx
@@ -2,21 +2,26 @@
 
 import Image from "next/image";
 import bookIcon from "@/app/assets/images/book_icon.svg";
-import { sendGAEvent } from "@next/third-parties/google";
+import { sendGAEvent, sendGTMEvent } from "@next/third-parties/google";
 
 export default function BuyButton() {
+  const sendEvents = () => {
+    sendGAEvent({
+      event: "BuyButtonClickedEvent_GA",
+      value: "BuyButtonClickedEventValue_GA",
+    });
+    sendGTMEvent({
+      event: "BuyButtonClickedEvent_GTM",
+      value: "BuyButtonClickedEventValue_GTM",
+    });
+  };
   return (
     <a
       href="https://a.co/d/fWogqk1"
       target="_blank"
       rel="noopener noreferrer"
       className="w-full sm:w-40 px-6 flex items-center justify-center gap-2 py-2 mt-4 text-secondary bg-goldenGradient border border-background rounded-3xl transition duration-300 hover:scale-105"
-      onClick={() =>
-        sendGAEvent({
-          event: "BuyButtonClickedEvent",
-          value: "BuyButtonClickedValue",
-        })
-      }
+      onClick={() => sendEvents()}
     >
       Adquira já
       <Image
